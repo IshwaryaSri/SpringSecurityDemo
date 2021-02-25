afcbb3c8ae94e3de104153c63563e7a22667be69
Ishwarya <ishwarya2189@gmail.com>
2021-02-25 12:50:37 +0530

    Spring security files added

---
+++ src/main/java/com/sample/demo/SpringBootDemo/AppSecurityConfig.java
@@ -0,0 +1,36 @@
+package com.sample.demo.SpringBootDemo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.HttpSecurityBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class AppSecurityConfig extends WebSecurityConfigurerAdapter
+{
+    @Autowired
+    private UserDetailsService usersService;
+
+    @Override
+    protected void configure(HttpSecurity http)throws Exception{
+       http.authorizeRequests().anyRequest().authenticated().and().formLogin().loginPage("/login").permitAll();
+    }
+
+    @Bean
+    public AuthenticationProvider authProvider()
+    {
+        DaoAuthenticationProvider provider =new DaoAuthenticationProvider();
+        provider.setUserDetailsService(usersService);
+        provider.setPasswordEncoder(new BCryptPasswordEncoder());
+        return provider;
+    }
+
+}
